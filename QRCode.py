35945137a5906d97b13ca1e792a8e7df5e45b60b
Francisco Perez <perezfg@vcu.edu>
2021-04-26 18:28:36 -0400

    Add it baby

--- QRCode.py
+++ QRCode.py
@@ -1,8 +1,18 @@
 import pyqrcode
+import png
+from pyqrcode import QRCode
 
-s = "whaddup fellas ur awesome"
 
+# String which represents the QR code
+s = "localhost"
+
+# Generate QR code
 url = pyqrcode.create(s)
 
-url.png("myqr.png", scale = 100)
+# Create and save the svg file naming "myqr.svg"
+# url.svg("myqr.svg", scale = 8)
+
+# Create and save the png file naming "myqr.png"
+url.png('myqr.png', scale = 6)
+
 url.show()
